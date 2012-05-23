dbf4b375e07bf422bfa3c3c57d0d22b3fa69b828
Patrick Gostovic <pg@phranq.com>
2012-05-23 16:53:08 -0400

    Added body function support. Changed from but.push() to buf[i++] = "bla" to support output expressions that span multiple escape sequences.

--- phnq_ejs.js
+++ phnq_ejs.js
@@ -4,7 +4,8 @@ require("phnq_log").exec("phnq_ejs", function(log)
 
 	phnq_core.assertServer();
 
-	var ESC_REGEX = /<%(=)?(.*?)%>\n?/g;
+	var ESC_REGEX = /<%(=)?([\s\S]*?)%>\n?/g;
+	var BODY_FN_REGEX = /([\s,])function\(([^)]*)\)\s*\{\s*$/;
 
 	var phnq_ejs =
 	{
@@ -21,9 +22,10 @@ require("phnq_log").exec("phnq_ejs", function(log)
 
 			var buf = [];
 			buf.push("(function(me){");
-			buf.push("var _buf=[];");
-			buf.push("with(me||{}){");
-			buf.push("var _out=function(str){_buf.push(str)};");
+			buf.push("me = me || {};");
+			buf.push("var _b=[];");
+			buf.push("var _i=0;");
+			buf.push("with(me){");
 
 			var m;
 			var s;
@@ -31,22 +33,35 @@ require("phnq_log").exec("phnq_ejs", function(log)
 			while((m = ESC_REGEX.exec(str)))
 			{
 				s = phnq_core.escapeJS(str.substring(idx, m.index));
-				buf.push(s ? "_out(\""+s+"\");" : "");
+				buf.push(s ? "_b[_i++]=\""+s+"\";" : "");
 
 				s = m[2].trim();
 				if(s)
 				{
+					// If it's a <%= %> expression then add the result to the buf
 					if(m[1])
-						buf.push("_out("+s+");");
+						buf.push("_b[_i++]=");
+
+					var bm = BODY_FN_REGEX.exec(s);
+					if(bm)
+					{
+						buf.push(s.substring(0, bm.index));
+						buf.push(bm[1]);
+						buf.push("function(_b){var _i=0;");
+					}
 					else
+					{
 						buf.push(s);
+						if(m[1])
+							buf.push(";");
+					}
 				}
 				idx = ESC_REGEX.lastIndex;
 			}
 			s = phnq_core.escapeJS(str.substring(idx));
-			buf.push(s ? "_out(\""+s+"\");" : "");
-			buf.push("}")
-			buf.push("return _buf.join(\"\");})");
+			buf.push(s ? "_b[_i++]=\""+s+"\";" : "");
+			buf.push("}");
+			buf.push("return _b.join(\"\");})");
 			return buf.join("");
 		}
 	};
